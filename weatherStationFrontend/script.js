5abc6dad9e98405ffedd5ced04634e39270155fd
maria <maria.terminasyan@gmail.com>
2025-01-13 00:04:09 +0100

    added report to all cards

--- weatherStationFrontend/script.js
+++ weatherStationFrontend/script.js
@@ -5,15 +5,6 @@ function showDateTime(){
     document.getElementById("dateTime").textContent = date;
 }
 
-function showTemp(){
-    document.getElementById("tempReport").style.display = "block";
-    document.getElementById("shadow").style.display = "block";
-}
-
-function hideTemp(){
-    document.getElementById("tempReport").style.display = "none";
-    document.getElementsById("shadow").style.display = "none";
-}
 //update every second
 setInterval(showDateTime, 1000);
 showDateTime();
